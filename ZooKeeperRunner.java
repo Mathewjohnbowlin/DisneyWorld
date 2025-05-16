f68798a86bd9f15ab967e723a373ad04893f2299
Mathewjohnbowlin <mathew_bowlin@students.nksd.net>
2025-05-16 07:23:21 -0400

    Add files via upload

---
+++ ZooKeeperRunner.java
@@ -0,0 +1,33 @@
+/*
+ * Activity 4.9.2
+ */
+
+public class ZooKeeperRunner
+{
+  public static void main(String[] args)
+  { 
+    Animal a = new Elephant("leaves, grasses, roots", false, 60.0);
+    a.isNocturnal();
+    //a.trumpet();
+
+    Animal a1 = new Animal();
+    a1.sleep();
+    //Object a2 = new Animal();
+    //a2.sleep();
+
+
+    Object o = new Object();
+    System.out.println(o.toString());
+    Elephant e = new Elephant("leaves, grasses, roots", false, 60.0);
+    System.out.println(e.toString());
+
+    Animal a3 = new Animal();
+    System.out.println(a3.toString());
+
+    Animal a4 = new Elephant("leaves, grasses, roots", false, 60.0);
+    System.out.println(a4.toString());
+
+    Object o2 = new Elephant("leaves, grasses, roots", false, 60.0);
+    System.out.println(o2.toString());
+  }
+}
